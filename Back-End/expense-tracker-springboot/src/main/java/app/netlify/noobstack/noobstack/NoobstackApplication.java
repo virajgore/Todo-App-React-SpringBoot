49eb909a29041718b7f5cf75590cb5cd6b02752c
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-08-06 04:14:40 +0800

    Expense tracker app with React and spring boot project started

---
+++ Back-End/expense-tracker-springboot/src/main/java/app/netlify/noobstack/noobstack/NoobstackApplication.java
@@ -0,0 +1,13 @@
+package app.netlify.noobstack.noobstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoobstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoobstackApplication.class, args);
+	}
+
+}
---
+++ Back-End/expense-tracker-springboot/src/test/java/app/netlify/noobstack/noobstack/NoobstackApplicationTests.java
@@ -0,0 +1,13 @@
+package app.netlify.noobstack.noobstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoobstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
